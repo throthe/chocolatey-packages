14ceba84be8f916308df18a66e199d553c3258fc
Thomas Rothe <th.rothe@live.de>
2026-07-20 09:43:33 +0800

    add noi to publish chocolatey packages

---
+++ published/noi/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'https://github.com/lencx/Noi/releases/download/v1.1.0/Noi.msi' 
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url64bit       = $url64
+  softwareName   = 'noi*'
+  checksum64     = '25780b556f6854c7906b3e339a807dbaf01c3892b05aa95c1bd6358495415346'
+  checksumType64 = 'sha256' #default is checksumType
+
+  # MSI
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ADDDESKTOPICON=1" 
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs 
---
+++ published/noi/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'noi*' 
+  fileType       = 'MSI'
+  silentArgs     = '/qn /norestart'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641) 
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | ForEach-Object {
+    $packageArgs['file'] = "$($_.UninstallString)"
+    $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$($packageArgs['packageName']) has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning 'To prevent accidental data loss, no programs will be uninstalled.'
+  Write-Warning 'Please alert package maintainer the following keys were matched:'
+  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
+}
